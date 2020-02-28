8c1064ffe6c112025bf830d279789a941f23664f
sum-123 <59158416+sum-123@users.noreply.github.com>
2020-02-28 19:35:35 +0800

    Create practice1.py

---
+++ February26/practice1.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : practice1.py
+@Time : 2020/02/28 19:30:00
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+mylist = [True, True, True]
+x = all(mylist)
+print(x)
+
+# Return True
